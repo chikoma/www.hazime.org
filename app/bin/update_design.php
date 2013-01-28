5a807762e9c013e3d2a2bf06d415ddf5e664ff9c
Hajime MATSUMOTO <mail@hazime.org>
2013-01-28 21:51:00 +0900

    add doc link

--- app/bin/update_design.php
+++ app/bin/update_design.php
@@ -1,25 +1,19 @@
 <?php
 // app/bin/update_design
-// =========================
+// ==================================================
 // デザインをアップデートする
-// =========================
+// ==================================================
 //
 //
-// =========================
+// ==================================================
 $dir = realpath(dirname(__FILE__)."/../"); // uper app dir 
 require_once dirname(__FILE__)."/../lib/header.php"; 
 
 echo "Updating Design\n\n";
 
-// Update Design
-
-// Layoutを取得する
-// =====================
-//$bs->website( )->parseLayout('index.html','main.html');
-
 //
 // 解析レシピ
-//
+// ==================================================
 $i = 0;
 $files[$i]['file']                = 'index.html';
 $files[$i]['blocks']['contents']  = 'index.html';
@@ -52,5 +46,5 @@ $i++;
 $bs->website()->importLayout( $layouts );
 $bs->website()->importContents( $files );
 
-echo "\n\n\n";
+echo "\n\n";
 ?>
