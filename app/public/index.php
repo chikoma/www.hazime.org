e00ceba4e87f29bd4de1ced1d063a5d89ae44f80
Hajime MATSUMOTO <mail@hazime.org>
2013-01-29 19:40:38 +0900

    delete public

--- app/public/index.php
+++ app/public/index.php
@@ -8,11 +8,5 @@ $website = $bs->website();
 
 // HTMLをビルドする
 // ------------------------
-$website->setVar('SUBTITLE','トップ');
-$website->addLayout('header','header.html');
-$website->addLayout('footer','footer.html');
-$website->addLayout('aside','top-profile.html');
-$website->addLayout('aside_adv','adv.html');
-$website->addLayout('contents','index.html');
-$website->build('main.html');
+echo file_get_contents(APP_DIR.'/design/index.html');
 ?>
